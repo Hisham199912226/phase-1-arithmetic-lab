c992d69be4179bf2e868f819db84b2268f69ff02
Hisham Al-Eesa <hisham.aleesa@gmail.com>
2023-09-25 13:57:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+/*Create a variable called multiply set to an equation that will multiply the variables num1 and num2; 
+the result of the multiplication should be 62. */
 
-// Write your code here
+let multiply, num1 = 6.2, num2 = 10;
+multiply = num1 * num2;   // 62
+
+/* Create a variable called random that will generate a random integer greater than 0. */
+let random = Math.floor(Math.random() * 10) + 1;
+
+/*Create a variable called mod set to an equation that will calculate the remainder of dividing variable num3 by num4; 
+the remainder should be 4. */
+let mod, num3 = 16, num4 = 12;
+mod = num3 % num4; // 4
+
+/* Create a variable called max that finds the highest number in a set; the value returned should be 20. */
+let max = Math.max(1,2,3,20,12,14,15);
